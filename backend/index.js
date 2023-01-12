42fa3add687a2e28d6d4ad01101138cba5d92c1c
Yash Tariyal <77398467+YashTariyal@users.noreply.github.com>
2023-01-12 21:00:51 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,47 @@
-const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors') 
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors')
 
-connectToMongo();
+const authRoute = require('./routes/auth')
+const notesRoute = require('./routes/notes')
+
+// express init
 const app = express()
-const port = 5000
+
+// mongoose init
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/inotebook'
+async function main() {
+  await mongoose.connect(dbUrl);
+  console.log("Database connected");
+}
+main().catch(err => console.log(err));
 
 app.use(cors())
+
+// middleware
 app.use(express.json())
 
-// Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hello wprld")
+})
+
+app.use('/api/auth', authRoute)
+app.use('/api/notes', notesRoute)
 
 
-app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
+// error handling middleware
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = 'Something went wrong'
+    res.status(statusCode).json({success: false, message: err.message}); //For development
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, (req, res) => {
+    console.log('Listening to the port 8080');
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,111 +1,24 @@
-const express = require('express');
-const User = require('../models/User');
-const router = express.Router();
-const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
-var fetchuser = require('../middleware/fetchuser');
-
-const JWT_SECRET = 'Harryisagoodb$oy';
-
-// ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
-router.post('/createuser', [
-  body('name', 'Enter a valid name').isLength({ min: 3 }),
-  body('email', 'Enter a valid email').isEmail(),
-  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-], async (req, res) => {
-  // If there are errors, return Bad request and the errors
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  try {
-    // Check whether the user with this email exists already
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ error: "Sorry a user with this email already exists" })
-    }
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(req.body.password, salt);
-
-    // Create a new user
-    user = await User.create({
-      name: req.body.name,
-      password: secPass,
-      email: req.body.email,
-    });
-    const data = {
-      user: {
-        id: user.id
-      }
-    }
-    const authtoken = jwt.sign(data, JWT_SECRET);
+const express = require("express");
+const {createUser, loginUser, getUser, forgotPassword, resetPassword} = require('../controllers/auth')
+const {validateUserRegister, validateUserLogin, fetchUser} = require('../middlewares')
+const catchAsync = require('../utils/catchAsync')
 
 
-    // res.json(user)
-    res.json({ authtoken })
+const router = express.Router()
 
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
-})
+// Create a new user using: POST /api/auth/createuser "no login required"
+router.post('/createuser', validateUserRegister, catchAsync(createUser))
 
+// authenticate a user using: POST /api/auth/login "no login required"
+router.post('/login', validateUserLogin, catchAsync(loginUser))
 
-// ROUTE 2: Authenticate a User using: POST "/api/auth/login". No login required
-router.post('/login', [
-  body('email', 'Enter a valid email').isEmail(),
-  body('password', 'Password cannot be blank').exists(),
-], async (req, res) => {
-  let success = false;
-  // If there are errors, return Bad request and the errors
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+// get logged in user details using: POST /api/auth/getuser "login required"
+router.post('/getuser',fetchUser, catchAsync(getUser))
 
-  const { email, password } = req.body;
-  try {
-    let user = await User.findOne({ email });
-    if (!user) {
-      success = false
-      return res.status(400).json({ error: "Please try to login with correct credentials" });
-    }
+// get logged in user details using: POST /api/auth/getuser "login required"
+router.post('/forgotpassword', catchAsync(forgotPassword))
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if (!passwordCompare) {
-      success = false
-      return res.status(400).json({ success, error: "Please try to login with correct credentials" });
-    }
+// get logged in user details using: POST /api/auth/getuser "login required"
+router.put('/resetpassword/:resetToken', catchAsync(resetPassword))
 
-    const data = {
-      user: {
-        id: user.id
-      }
-    }
-    const authtoken = jwt.sign(data, JWT_SECRET);
-    success = true;
-    res.json({ success, authtoken })
-
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
-
-
-});
-
-
-// ROUTE 3: Get loggedin User Details using: POST "/api/auth/getuser". Login required
-router.post('/getuser', fetchuser,  async (req, res) => {
-
-  try {
-    userId = req.user.id;
-    const user = await User.findById(userId).select("-password")
-    res.send(user)
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
-})
 module.exports = router
